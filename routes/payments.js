f243c67da325bdca8de944aff15aee31d4f043e8
AhmadJilani12 <142293908+AhmadJilani12@users.noreply.github.com>
2026-03-09 17:33:46 +0500

    Update payments.js

--- routes/payments.js
+++ routes/payments.js
@@ -2,8 +2,8 @@ const router = require('express').Router();
 const { createStripeIntent, confirmStripePayment, processCod } = require('../controllers/paymentController');
 const { verifyToken } = require('../middleware/auth');
 
-router.post('/stripe', verifyToken, createStripeIntent);
-router.post('/stripe/confirm', verifyToken, confirmStripePayment);
+router.post('/stripe', createStripeIntent);
+router.post('/stripe/confirm',  confirmStripePayment);
 router.post('/cod', verifyToken, processCod);
 
 module.exports = router;
